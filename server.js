d9fc6e6048732ef03d9a516fac934f89959abb6f
Camilla Scarton <camy.s96@gmail.com>
2023-05-25 20:13:12 +0200

    body parser, error constants, error handler

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require("express");
+const errorHandler = require("./middlewares/errorHandler");
 const dotenv = require("dotenv").config();
 
 const port = process.env.PORT || 5000;
 
 const app = express();
 
+app.use(express.json()); // parser for body requests
 app.use("/api/contacts", require("./routes/contactRoutes"));
+app.use(errorHandler);
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
