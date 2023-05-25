935248aca0a84147e217be712c24d7b70412d6f0
Camilla Scarton <camy.s96@gmail.com>
2023-05-25 20:26:41 +0200

    async functions handler; details for memo in readme

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,46 +1,49 @@
+// to wrap async functions and handle the try/catch automatically
+const asyncHandler = require("express-async-handler");
+
 /**
  * @description Get all contacts
  * @route GET /api/contacts
  * @access public
  */
-const getContacts = (req, res) => {
+const getContacts = asyncHandler(async (req, res) => {
     res.status(200).json({ message: `Get all contacts` });
-}
+})
 
 /**
  * @description Get contact by id
  * @route GET /api/contacts/:id
  * @access public
  */
-const getContactById = (req, res) => {
+const getContactById = asyncHandler(async (req, res) => {
     res.status(200).json({ message: `Get contact for ${req.params.id}` });
-}
+})
 
 /**
  * @description Create new contact
  * @route POST /api/contacts/
  * @access public
  */
-const createContact = (req, res) => {
+const createContact = asyncHandler(async (req, res) => {
     res.status(201).json({ message: "Create contact" });
-}
+})
 
 /**
  * @description Update contact by id
  * @route PUT /api/contacts/:id
  * @access public
  */
-const updateContactById = (req, res) => {
+const updateContactById = asyncHandler(async (req, res) => {
     res.status(200).json({ message: `Update contact for ${req.params.id}` });
-}
+})
 
 /**
  * @description Delete contact by id
  * @route DELETE /api/contacts/:id
  * @access public
  */
-const deleteContactById = (req, res) => {
+const deleteContactById = asyncHandler(async (req, res) => {
     res.status(200).json({ message: `Delete contact for ${req.params.id}` });
-}
+})
 
 module.exports = { getContacts, getContactById, createContact, updateContactById, deleteContactById };
--- readme.md
+++ readme.md
@@ -1,3 +1,10 @@
 # Contacts manager API project
 
 - Video inspo: https://www.youtube.com/watch?v=H9M02of22z4
+
+## Details
+
+- express for server
+- nodemon to reload server on files saving
+- dotenv to use .env file
+- express-async-handler to wrap async functions and use try/catch block without writing it
