75d77c4645394dde1c7729eee1a99b12b41e6c11
Camilla Scarton <camy.s96@gmail.com>
2023-05-26 11:20:36 +0200

    mongodb cluster + moongoose configuration

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const errorHandler = require("./middlewares/errorHandler");
+const connectDb = require("./config/dbConnection");
 const dotenv = require("dotenv").config();
 
 const port = process.env.PORT || 5000;
 
+connectDb();
 const app = express();
 
 app.use(express.json()); // parser for body requests
